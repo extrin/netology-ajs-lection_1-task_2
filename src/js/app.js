1d294bc99cacb78ed3ef7d759a534327be507554
E.S <extrin13@gmail.com>
2019-03-27 23:00:39 +0300

    feat: Add functions and script

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+const get_plural_form = (value, form1 = "балл", form2 = "балла", form3 = "баллов") => {
+  "use strict";
+  if (!isNaN(value)) {
+    if (value === 0) return
+    const remainder_1 = Math.abs(value) % 100;
+    const remainder_2 = value % 10;
+    if ((remainder_1 > 10) && (remainder_1 < 20)) return form3;
+    if ((remainder_2 > 1) && (remainder_2 < 5)) return form2;
+    if (remainder_2 === 1) return form1;
+  } else {
+    console.log("01 Input is not a number");
+    return 0;
+  }
+}
+
+const set_message = (value) => `Ваш баланс: ${value} ${get_plural_form(value)}`;
+
+const sample_data = [523, 6000, 5001, 5013];
+for (let value of sample_data) {
+  document.getElementById(`data_${value}`).innerHTML = value;
+  document.getElementById(`res_${value}`).innerHTML = set_message(value);
+}
