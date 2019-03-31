9edc8b3ac2d525b3aec61610259d6c079ac0d146
E.S <extrin13@gmail.com>
2019-03-31 19:40:17 +0300

    fix: Add zero case

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,9 @@ const get_plural_form = (value, form1 = "балл", form2 = "балла", form3 
     const remainder_2 = value % 10;
     if ((remainder_1 > 10) && (remainder_1 < 20)) return form3;
     if ((remainder_2 > 1) && (remainder_2 < 5)) return form2;
+    if ((remainder_1 > 10 && remainder_1 < 20) || remainder_1 === 0)
+      return form3;
+    if (remainder_2 > 1 && remainder_2 < 5) return form2;
     if (remainder_2 === 1) return form1;
   } else {
     console.log("01 Input is not a number");
