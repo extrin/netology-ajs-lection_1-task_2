2d85b5793d4da4d44c87c2c14aa18f0a38731293
E.S <extrin13@gmail.com>
2019-03-31 19:45:57 +0300

    chore: Linter fixings

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
-const get_plural_form = (value, form1 = "балл", form2 = "балла", form3 = "баллов") => {
-  "use strict";
+const get_plural_form = (
+  value,
+  form1 = "балл",
+  form2 = "балла",
+  form3 = "баллов"
+) => {
   if (!isNaN(value)) {
-    if (value === 0) return
     if (value === 0) return form3;
     const remainder_1 = Math.abs(value) % 100;
     const remainder_2 = value % 10;
-    if ((remainder_1 > 10) && (remainder_1 < 20)) return form3;
-    if ((remainder_2 > 1) && (remainder_2 < 5)) return form2;
     if ((remainder_1 > 10 && remainder_1 < 20) || remainder_1 === 0)
       return form3;
     if (remainder_2 > 1 && remainder_2 < 5) return form2;
@@ -15,9 +16,10 @@ const get_plural_form = (value, form1 = "балл", form2 = "балла", form3 
     console.log("01 Input is not a number");
     return 0;
   }
-}
+};
 
-const set_message = (value) => `Ваш баланс: ${value} ${get_plural_form(value)}`;
+// ВОТ ЗДЕСЬ ИСПОЛЬЗУЮТСЯ ШАБЛОННЫЕ СТРОКИ
+const set_message = value => `Ваш баланс: ${value} ${get_plural_form(value)}`;
 
 const sample_data = [523, 6000, 5001, 5013];
 for (let value of sample_data) {
